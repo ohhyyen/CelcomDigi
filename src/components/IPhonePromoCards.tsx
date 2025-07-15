4f6df880944e504ee7f3d5cb2a636cb7b84b7ab1
[dyad] <ohhy11d@gmail.com>
2025-07-15 22:51:33 +0800

    [dyad] Menambahkan harga untuk semua model iPhone dalam IPhonePromoCards.tsx. - wrote 1 file(s)

--- src/components/IPhonePromoCards.tsx
+++ src/components/IPhonePromoCards.tsx
@@ -10,9 +10,9 @@ interface IPhonePromoCardsProps {
 const IPhonePromoCards: React.FC<IPhonePromoCardsProps> = ({ onSelectIPhone }) => {
   const iPhones = [
     { name: 'iPhone 16 Pro Max', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66dfee42b7a243aab721a37f_CelcomDigi_iPhone-16-Pro_Device-Image.avif', price: '(RM600)' },
-    { name: 'iPhone 16 Pro', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66dfee428f6c2ccb7da97d60_CelcomDigi_iPhone-16-Pro-Max_Device-Image.avif' },
-    { name: 'iPhone 16', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66e2613e9aff0f9a20c4ca03_CelcomDigi_iPhone-16-Pink_Device-Image.avif' },
-    { name: 'iPhone 16e', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/67bfeb741c4477ee5c1a6f7a_CelcomDigi_iPhone16e_device_image.avif' },
+    { name: 'iPhone 16 Pro', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66dfee428f6c2ccb7da97d60_CelcomDigi_iPhone-16-Pro-Max_Device-Image.avif', price: '(RM550)' },
+    { name: 'iPhone 16', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66e2613e9aff0f9a20c4ca03_CelcomDigi_iPhone-16-Pink_Device-Image.avif', price: '(RM500)' },
+    { name: 'iPhone 16e', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/67bfeb741c4477ee5c1a6f7a_CelcomDigi_iPhone16e_device_image.avif', price: '(RM450)' },
   ];
 
   return (

97d56fbc8b13f9d8f57ccc93b1962ad27a2c0091
[dyad] <ohhy11d@gmail.com>
2025-07-15 22:38:40 +0800

    Reverted all changes back to version 177212ab522a48c585f79aa22daafe5cc8496d99

--- src/components/iPhonePromotionList.tsx
+++ src/components/iPhonePromotionList.tsx
@@ -7,7 +7,7 @@ interface iPhonePromotionListProps {
   onSelectIPhone: (iphone: { name: string; image: string; }) => void;
 }
 
-export function iPhonePromotionList({ onSelectIPhone }: iPhonePromotionListProps) {
+export const iPhonePromotionList: React.FC<iPhonePromotionListProps> = ({ onSelectIPhone }) => {
   const iPhones = [
     { name: 'iPhone 16 Pro Max', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66dfee42b7a243aab721a37f_CelcomDigi_iPhone-16-Pro_Device-Image.avif' },
     { name: 'iPhone 16 Pro', image: 'https://cdn.prod.website-files.com/637c596725cb8b7f82fe360b/66dfee428f6c2ccb7da97d60_CelcomDigi_iPhone-16-Pro-Max_Device-Image.avif' },
@@ -31,4 +31,4 @@ export function iPhonePromotionList({ onSelectIPhone }: iPhonePromotionListProps
       ))}
     </div>
   );
-}
+};

c85f6d9f9d3d66a5f18ece522aea3e44c6ff789d
[dyad] <ohhy11d@gmail.com>
2025-07-18 03:30:45 +0800

    [dyad] Mengemas kini saiz logo dan meminta pengguna untuk memeriksa konsol pembangun pelayar untuk ralat. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-celcomdigi-dark-blue text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="flex items-center">
-          <img src="/231020-celcomdigi-logo-1140x570.jpg" alt="CelcomDigi Logo" className="h-8" />
+          <img src="/231020-celcomdigi-logo-1140x570.jpg" alt="CelcomDigi Logo" className="h-10 w-auto" /> {/* Increased height to h-10 and added w-auto */}
         </Link>
         <div className="flex space-x-4">
           <Link to="/postpaid" className="hover:text-celcomdigi-light-blue">Pascabayar</Link>

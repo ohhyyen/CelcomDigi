85dc1d169cfe30b957798c4867bb50f81e5f6721
[dyad] <ohhy11d@gmail.com>
2025-07-18 03:53:37 +0800

    [dyad] Membantu pengguna menyelesaikan ralat `net::ERR_NAME_NOT_RESOLVED` dengan mengesyorkan penyelesaian masalah rangkaian dan mengembalikan kod logo. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,8 +6,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-celcomdigi-dark-blue text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="flex items-center">
-          {/* Menggunakan imej placeholder untuk diagnosis */}
-          <img src="https://via.placeholder.com/150x50?text=Logo+Placeholder" alt="CelcomDigi Logo Placeholder" className="h-12 w-auto" />
+          <img src="/231020-celcomdigi-logo-1140x570.jpg" alt="CelcomDigi Logo" className="h-12 w-auto" />
         </Link>
         <div className="flex space-x-4">
           <Link to="/postpaid" className="hover:text-celcomdigi-light-blue">Pascabayar</Link>

6289fb38832568b75fb5620dfa6e91f75a750b8f
[dyad] <ohhy11d@gmail.com>
2025-07-18 11:53:23 +0800

    [dyad] Mengemas kini Navbar untuk saiz yang lebih kecil dan logo yang lebih besar pada paparan mudah alih. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-celcomdigi-dark-blue text-white h-24 sm:h-60 px-4 shadow-md flex items-center"> {/* Menetapkan ketinggian yang lebih kecil untuk mudah alih */}
+    <nav className="bg-celcomdigi-dark-blue text-white h-16 sm:h-60 px-4 shadow-md flex items-center"> {/* Menetapkan ketinggian yang lebih kecil untuk mudah alih */}
       <div className="container mx-auto flex justify-between items-center h-full">
         <Link to="/" className="flex items-center h-full">
-          <img src="/celcomdigi-logo.jpg" alt="CelcomDigi Logo" className="h-16 sm:h-full w-auto object-contain" /> {/* Saiz logo responsif */}
+          <img src="/celcomdigi-logo.jpg" alt="CelcomDigi Logo" className="h-full w-auto object-contain" /> {/* Saiz logo responsif, kini akan mengisi ketinggian navbar */}
         </Link>
         <div className="hidden md:flex space-x-4"> {/* Sembunyikan pautan pada mudah alih, tunjukkan pada md ke atas */}
           <a href="https://www.celcomdigi.com/postpaid" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">Pascabayar</a>

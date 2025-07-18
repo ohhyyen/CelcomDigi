f011b172fc904a73103bd040e6689bcca5bdf26a
[dyad] <ohhy11d@gmail.com>
2025-07-18 10:27:11 +0800

    [dyad] Mengemas kini pautan navigasi untuk Fiber, Peranti, dan Promosi ke URL luaran. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,11 +12,11 @@ const Navbar: React.FC = () => {
         <div className="flex space-x-4">
           <a href="https://www.celcomdigi.com/postpaid" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">Pascabayar</a>
           <a href="https://www.celcomdigi.com/prepaid/nx" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">Prabayar</a>
-          <Link to="/fibre" className="hover:text-celcomdigi-light-blue">Fiber</Link>
-          <Link to="/devices/apple" className="hover:text-celcomdigi-light-blue">Peranti</Link>
+          <a href="https://www.celcomdigi.com/fibre" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">Fiber</a>
+          <a href="https://www.celcomdigi.com/devices/phone-care" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">Peranti</a>
           <Link to="/lifestyle" className="hover:text-celcomdigi-light-blue">Gaya Hidup</Link>
           <Link to="/roaming" className="hover:text-celcomdigi-light-blue">Perayauan</Link>
-          <Link to="/promotions" className="hover:text-celcomdigi-light-blue">Promosi</Link>
+          <a href="https://www.celcomdigi.com/promotions" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">Promosi</a>
           <a href="https://help.celcomdigi.com/" target="_blank" rel="noopener noreferrer" className="hover:text-celcomdigi-light-blue">DAPATKAN BANTUAN</a>
         </div>
       </div>
